92da07954e4941ffc737a0f671cab925b50ae55a
Atif Javed <atif@gems.techverx.com>
2016-05-06 22:40:21 +0500

    fix crash on admin side

--- app/models/shoppe/order_item.rb
+++ app/models/shoppe/order_item.rb
@@ -111,7 +111,7 @@ module Shoppe
     # @return [BigDecimal]
     def unit_price
       # read_attribute(:unit_price) || ordered_item.try(:price) || BigDecimal(0)
-      read_attribute(:unit_price) || ordered_item.get_price || BigDecimal(0)
+      read_attribute(:unit_price) || ordered_item.try(:get_price) || BigDecimal(0)
     end
 
     # The cost price for the item
