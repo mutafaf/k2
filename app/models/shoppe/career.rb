d85c239ea0b2f011cbfcb7b3791c34114469a467
qubaish-techverx <qubaish@gems.techverx.com>
2016-05-17 23:01:45 +0500

    add career file

--- app/models/shoppe/career.rb
+++ app/models/shoppe/career.rb
@@ -3,6 +3,14 @@ class Shoppe::Career < ActiveRecord::Base
 
   has_many :attachments, as: :parent, dependent: :destroy, autosave: true, class_name: 'Shoppe::Attachment'
 
+  GENDER = [['Male', 'Male'], ['Female', 'Female']]
+  MARTIAL_STATUS = [['Martial Status', ''],['Single', 'Single'], ['Married', 'Married']]
+  HIGHEST_DEGREE = [["Highest Degree", ""], ["Computer IT", "Computer IT"], ["Master", "Master"]]
+  INSTITUE = [["Institute", ""], ["LUMS", "LUMS"], ["FAST", "FAST"], ["COMSATS", "COMSATS"]]
+  YEAR_OF_COMPLETION = [["Year of Completion", ""], ["2000", "2000"], ["2001", "2001"]]
+  RELEVANT_EXPERICE = [["Relevant Experice", ""], ["1", "1"], ["2", "2"]]
+  DESIGNATION = [["Title / Designation", ""], ["LUMS", "LUMS"], ["FAST", "FAST"], ["COMSATS", "COMSATS"]]
+
   def attachments=(attrs)
     if attrs['default_image']['file'].present? then attachments.build(attrs['default_image']) end
   end
