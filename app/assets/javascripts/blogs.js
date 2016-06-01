51b6d9c556f8286e01b8c0b7796a9094aa45f086
Atif Javed <atif@gems.techverx.com>
2016-06-01 16:36:59 +0500

    Adde social share buttons

--- app/assets/javascripts/blogs.js
+++ app/assets/javascripts/blogs.js
@@ -1,5 +1,32 @@
+
 function fbShare(url, title, descr, image, winWidth, winHeight) {
     var winTop = (screen.height / 2) - (winHeight / 2);
     var winLeft = (screen.width / 2) - (winWidth / 2);
     window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
 }
+
+function googleplusbtn(url) {
+  sharelink = "https://plus.google.com/share?url="+url;
+  newwindow=window.open(sharelink,'name','height=400,width=600');
+  if (window.focus) {newwindow.focus()}
+  return false;
+}
+
+$(document).ready(function() {
+  $('.twitterShare').click(function(event) {
+    var width  = 575,
+        height = 400,
+        left   = ($(window).width()  - width)  / 2,
+        top    = ($(window).height() - height) / 2,
+        url    = this.href,
+        opts   = 'status=1' +
+                 ',width='  + width  +
+                 ',height=' + height +
+                 ',top='    + top    +
+                 ',left='   + left;
+
+    window.open(url, 'twitter', opts);
+
+    return false;
+  });
+});
