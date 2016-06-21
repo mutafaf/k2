af2da9bb5520620061d76df72fcd4c8555e9e46e
Atif Javed <atif@gems.techverx.com>
2016-06-21 22:56:20 +0500

    Changes for careees

--- app/models/shoppe/job.rb
+++ app/models/shoppe/job.rb
@@ -3,6 +3,7 @@ class Shoppe::Job < ActiveRecord::Base
   STATUS = [['ENABLED', 1], ['DISABLED', 0]]
   validates :name, :description, presence: true, uniqueness: true
   scope :enabled, -> { where(status: true) }
+  scope :last_15_days, -> { where("created_at > ?", 15.days.ago) }
   has_many :careers
 
 end
