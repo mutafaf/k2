5862b0b9b7df7587ac261e5e516380c1a3289216
Atif Javed <atif@gems.techverx.com>
2016-06-28 23:37:59 +0500

    removed extra code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-
-  def show_delivery_charges(order)
-    if order.delivery_charges > 0
-      return number_to_currency order.delivery_charges
-    else
-      return "Free"
-    end
-  end
-
 end
