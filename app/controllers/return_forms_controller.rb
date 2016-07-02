932e100c7ee28a98bcc9425534d37e33b1bf6899
Atif Javed <atif@gems.techverx.com>
2016-07-03 01:01:01 +0500

    required login for return form

--- app/controllers/return_forms_controller.rb
+++ app/controllers/return_forms_controller.rb
@@ -1,4 +1,5 @@
 class ReturnFormsController < ApplicationController
+  before_filter :authenticate_user!
   before_action :set_return_form, only: [:show, :edit, :update, :destroy]
 
   # GET /return_forms
