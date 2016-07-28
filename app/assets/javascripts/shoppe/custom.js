6cd2a0833026608cd46840796308456b9ae08707
Atif Javed <atif@gems.techverx.com>
2016-07-29 00:19:29 +0500

    indent code

--- app/assets/javascripts/shoppe/custom.js
+++ app/assets/javascripts/shoppe/custom.js
@@ -1,29 +1,19 @@
 $(document).ready(function(){
-	
+  var k = $("#colorbox").val();
 
-			var k = $("#colorbox").val();
-
-				if (k == "Multi") {
-					$("#color-field").hide();
-								
-									} 
-					else {
-							$("#color-field").show()
-						
-
-						}
+  if (k == "Multi") {
+    $("#color-field").hide();
+  } 
+  else {
+      $("#color-field").show()
+  }
   $("#colorbox").keyup(function(){
-       
-			var n = $("#colorbox").val();	
-
-				if (n == "Multi") {
-							
-							$("#color-field").hide();
-									} 
-				else {
-							$("#color-field").show()
-					}
-
-    			});
-
-   });
+    var n = $("#colorbox").val(); 
+    if (n == "Multi") {
+      $("#color-field").hide();
+          } 
+    else {
+      $("#color-field").show()
+      }
+  });
+});
