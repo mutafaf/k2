a661e7efa21bd566c72e70836dc68691bd3fba6b
Atif Javed <atif@gems.techverx.com>
2016-09-23 17:30:51 +0500

    added a random string to product category

--- app/controllers/shoppe/product_categories_controller.rb
+++ app/controllers/shoppe/product_categories_controller.rb
@@ -37,7 +37,9 @@ module Shoppe
         redirect_to :product_categories, flash: { alert: t('shoppe.product_category.destroy_alert2') }
       else
         @product_category.status = "deleted"
+        @product_category.permalink = [@product_category.permalink,"-",SecureRandom.hex(4)].join
         @product_category.save
+
         redirect_to :product_categories, flash: { notice: t('shoppe.product_category.destroy_notice') }
       end
     end
